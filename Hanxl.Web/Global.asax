b33370cc070acbe62a60e072e7f4801fcb31c554
hanxiaoliang <510692737@qq.com>
2016-09-09 21:11:17 +0800

    添加项目文件。

---
+++ Hanxl.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Hanxl.Web.MvcApplication" Language="C#" %>
---
+++ Hanxl.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Hanxl.Web.Startup))]
+namespace Hanxl.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
